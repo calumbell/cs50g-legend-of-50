4c36802f85f98a58f4d14c7c03a94167827ba66e
Calum Bell <calumabell@googlemail.com>
2020-08-01 20:38:29 +0100

    fixed bug /w dead entities triggering particles

--- src/states/entity/player/PlayerSwingSwordState.lua
+++ src/states/entity/player/PlayerSwingSwordState.lua
@@ -58,7 +58,7 @@ end
 function PlayerSwingSwordState:update(dt)
     -- check if hitbox collides with any entities in the scene
     for k, entity in pairs(self.dungeon.currentRoom.entities) do
-        if entity:collides(self.swordHitbox) then
+        if entity:collides(self.swordHitbox) and not entity.dead then
             entity:damage(1)
             gSounds['hit-enemy']:play()
             self.dungeon.currentRoom.psystems['hit-entity']:spawnParticles(entity.x, entity.y)
